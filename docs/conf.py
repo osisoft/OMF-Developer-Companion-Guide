17cf386365d8128dbbcced95b89363bb4f138bc9
OSI-Writer <bbelisle@osisoft.com>
2018-03-26 13:15:37 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -225,7 +225,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'qidocs.tex', 'OSIsoft Qi Documentation',
+  ('index', 'qidocs.tex', 'OMF Developer's Companion Guide',
    'OSIsoft', 'manual'),
 ]
 
@@ -255,7 +255,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'Qi', 'OSIsoft Qi Documentation',
+    ('index', 'Qi', 'OMF Developer's Companion Guide',
      ['OSIsoft'], 1)
 ]
 
@@ -269,8 +269,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'qi', 'OSIsoft Qi Documentation',
-   'OSIsoft', 'Qi', 'OSIsoft Qi Documentation',
+  ('index', 'qi', 'OMF Developer's Companion Guide',
+   'OSIsoft', 'Qi', 'OMF Developer's Companion Guide',
    'Qi Development'),
 ]
 
